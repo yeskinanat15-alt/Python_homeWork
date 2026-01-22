bcc22774b284e506ade765b55bf9fb329739481c
Natalya Eskina <yeskinanat15@gmail.com>
2026-01-22 19:39:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('1111')
+    print_hi('999')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
